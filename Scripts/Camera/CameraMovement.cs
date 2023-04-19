01d5e5bc562eb2f8e18ffb7ede33548869093c1b
YuriiPurdenko <yurii.purdenko@gmail.com>
2023-04-19 19:18:58 +0300

    signed commit

--- Scripts/Camera/CameraMovement.cs
+++ Scripts/Camera/CameraMovement.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
-    Vector3 startPos = new Vector3(-0.87f, 8.07f, -17.74f);
-    Vector3 offset = new Vector3(0, 3, -8);
     Vector3 startRotation = new Vector3(3.77f ,3.246f,0.214f);
     Vector3 rotation = new Vector3(16f ,3.246f,0.214f); 
+    Vector3 startPos = new Vector3(-0.87f, 8.07f, -17.74f);
+    Vector3 offset = new Vector3(0, 3, -8);
+    
     void Start()
     {
         transform.position = startPos;
